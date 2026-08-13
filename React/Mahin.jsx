345d282fab07d6abea61087de5cd0258a79a5249
CAROSINE <moshiur287rahman@gmail.com>
2026-08-13 11:03:25 +0600

    react

---
+++ React/Mahin.jsx
@@ -0,0 +1,11 @@
+import Mahin from './Mahin.jsx'
+function Mahin() {
+  return (
+    <div>
+      <h1>Welcome, Mahin!</h1>
+      <p>This is my first React JSX component.</p>
+    </div>
+  );
+}
+
+export default Mahin;
