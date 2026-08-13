3f5c1c24eaf7383927ed713338799748559f0414
CAROSINE <moshiur287rahman@gmail.com>
2026-08-14 00:52:23 +0600

    ^_^

---
+++ TypeScript/array.ts
@@ -0,0 +1,8 @@
+let skills: string[] = ["React", "TS"]
+
+const user = 
+{
+  name: "John",    // actual value দিতে হবে
+  age: 21          // actual value দিতে হবে
+}
+ 
---
+++ TypeScript/variable.ts
@@ -0,0 +1,9 @@
+let userName: string = "John"
+let age: number = 21
+let isOnline: boolean = true
+
+console.log(userName)
+
+console.log(age)
+
+console.log(isOnline)
